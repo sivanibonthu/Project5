692ccbf4c6193b8074066ee85aadd326cdd65891
bonth <bonth@VISHNU_PRIYA>
2023-09-27 16:47:00 +0530

    First Commit

---
+++ project10/src/com/tns/constructorprog/ConstructorDemo.java
@@ -0,0 +1,29 @@
+package com.tns.constructorprog;
+
+import java.util.Scanner;
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner sc=new Scanner(System.in);
+      String name,city;
+      int id;
+      System.out.println("enter customer id");
+      id=sc.nextInt();
+      sc.nextLine();
+      
+      System.out.println("enter customer name");
+      name=sc.nextLine();
+      
+      System.out.println("enter customer city");
+      city=sc.nextLine();
+      Customer c1=new Customer(name,id,city);
+  //    c1.setCustomerId(id);
+  //    c1.setCustomerName(name);
+  //     c1.setCustomerCity(city);
+      System.out.println(c1);
+      
+	}
+
+}
