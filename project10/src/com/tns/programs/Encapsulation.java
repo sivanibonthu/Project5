bca27b6419e05b72f2dd1608838644ba2cee35d5
bonth <bonth@VISHNU_PRIYA>
2023-09-27 15:58:12 +0530

    First Commit

---
+++ project10/src/com/tns/programs/Encapsulation.java
@@ -0,0 +1,13 @@
+package com.tns.programs;
+
+public class Encapsulation {
+      // data members
+	public int serialno;
+	public String name;
+	public int age;
+	//member function
+	public void show()
+	{
+		System.out.println("serialno"+serialno+"\t"+"name"+name+"\t"+"age"+age);
+	}
+}
---
+++ project10/src/com/tns/programs/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tns.programs;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Encapsulation ob= new Encapsulation();
+     ob.serialno=33;
+     ob.name="navya";
+     ob.age=21;
+     ob.show();
+	}
+
+}
